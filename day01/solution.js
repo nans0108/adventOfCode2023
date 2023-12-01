20f16366f8fe938d1227d7388cc68d5eaddd9134
nans0108 <rosicki.1995@gmail.com>
2023-12-01 10:46:42 +0100

    day1 first part of solution

---
+++ day01/solution.js
@@ -0,0 +1,32 @@
+const readFile = () => {
+  const fs = require("fs");
+
+  const fileName = "input.txt";
+
+  fs.readFile(fileName, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Error reading file ${fileName}: ${err.message}`);
+    } else {
+      const lines = data.split("\n");
+      const twoDigitsArray = convertLinesToTwoDigit(lines);
+      console.log(sum(twoDigitsArray));
+    }
+  });
+};
+
+const convertLinesToTwoDigit = (data) => {
+  const newData = data.map((line) => {
+    const firstInt = line.match(/\d/);
+    const secondInt = line.split("").reverse().join("").match(/\d/);
+    return secondInt ? `${firstInt}${secondInt}` : `${firstInt}${firstInt}`;
+  });
+  return newData;
+};
+
+const sum = (data) => {
+  return data.reduce((acc, num) => {
+    return acc + parseInt(num, 10);
+  }, 0);
+};
+
+readFile();
