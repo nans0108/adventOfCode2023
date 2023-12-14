93310dc10771c42bb2aac2f623ee65612d125b55
Sara Rosicki <sara.rosicki@rsqtechnologies.com>
2023-12-14 23:01:37 +0100

    Add day 12 solution for first part

---
+++ day12/solution.js
@@ -0,0 +1,54 @@
+const readFile = () => {
+  const fs = require("fs");
+
+  const fileName = "input.txt";
+
+  fs.readFile(fileName, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Error reading file ${fileName}: ${err.message}`);
+    } else {
+      const lines = data.split("\n");
+      let firstCounter = 0;
+
+      lines.map((line) => {
+        const data = line.split(" ");
+        firstCounter =
+          firstCounter +
+          getArrangementPossibilities(data[0], data[1].split(","));
+      });
+
+      console.log("firstCounter: ", firstCounter);
+    }
+  });
+};
+
+const getArrangementPossibilities = (code, arrangement) => {
+  let counter = 0;
+  const numberOfQuestion = (code.match(/\?/g) || []).length;
+  const possibilities = Math.pow(2, numberOfQuestion);
+
+  for (let i = 0; i < possibilities; i++) {
+    let binary = i.toString(2).padStart(numberOfQuestion, "0");
+    let positionCounter = 0;
+    const combination = code.replace(/\?/g, () => {
+      const replacement = binary.charAt(positionCounter) === "1" ? "#" : ".";
+      positionCounter++;
+      return replacement;
+    });
+    const arrayOfHash = combination.split(".").filter((e) => e !== "");
+
+    let tempCounter = 0;
+    for (let j = 0; j < arrayOfHash.length; j++) {
+      if (arrayOfHash.length === arrangement.length) {
+        if (arrayOfHash[j].length == arrangement[j]) {
+          tempCounter++;
+        }
+      }
+    }
+    if (tempCounter === arrangement.length) counter++;
+  }
+
+  return counter;
+};
+
+readFile();
